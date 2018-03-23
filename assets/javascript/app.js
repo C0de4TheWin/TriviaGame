4071628dad8d8cd8427dafe3ba4f376cd049d051
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-03-22 19:39:13 -0700

    bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,45 @@
+$(document).ready(onReady);
+
 var counter = 0;
-var time = 0;
-var timerCounter = 30;
-//var empireQ = ["array"]
+var userPick
+var wrongAnswer
+var rightAnswer = 0;  //global variables
+var timeClock = 0;
+var currentQuestion = 0;
 
-//first we have to call the function on click event
-//so upon click, the questions are displayed with timer
-$("#startGame").on("click", function(event) {
 
-$("#startGame").hide();
-
-var musicQuestion = [ {
-     question: "How many Stormtroopers died in the Death Star Explosion?",
-     answers: ["20,000", "30,000", "40,000", "50,000"],
-     rightAnswer: 1
+var empireQuestion = [{
+    question: "How many Stormtroopers died in the Death Star Explosion?",
+    answers: ["20,000", "30,000", "40,000", "50,000"],
+    rightAnswer: 1,
 
 }, {
 
     question: "Why was the clone trooper program phased out for Stormtroopers?",
     answers: ["Cost", "Loyalty", "Better Armor", "low on recruits"],
+    rightAnswer: 1,
 
 
 }, {
     question: "How many years of training does it take at the Carida Acadamy to become a Stormtrooper?",
     answers: ["1.5", "3", "5", "2"],
+    rightAnswer: 3,
 
 
 }, {
-    question: "How many blas resistant pieces make up Stormtrooper armor?",
+    question: "How many blast resistant pieces make up Stormtrooper armor?",
 
-    answers: ["12", "18", "55", "25"]
-
-
-
-}
-    
+    answers: ["12", "18", "55", "25"],
+    rightAnswer: 1.
 
 
 
-}
+}, {
+    question: "How many colonies are within the control of the Empire?",
 
+    answers: ["69 million", "50 million", "25 million", "35 million"],
+    rightAnswer: 0,
 
-
-
-
-
-$("#quiz-area").html("<h2> </h2>");
-       $("#trivia").html("<h2>QUESTIONS GO HERE</h2>");
-    clock();
-    generateHTML();
-
-});
-
+},
 //Upon start of game we must set the timer countdown here
 
-
-//upon completion of survay or timer expiration
-
-//results are displayed correct/incorrec and prompt is given
